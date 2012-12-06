614b06b58e8c2cb7acf7defa72cf1d6579fef4a0
ztl <ztl2004@gmail.com>
2012-12-06 06:15:22 -0800

    a process-based version. did not work either.

--- c1/B.py
+++ c1/B.py
@@ -1,38 +1,30 @@
-def tToa(zero, base, number):
-	ret = '' 
-	while number:
-		digital = (number-1)%base
-		ret = chr(digital+zero) + ret
-		number = (number-1)/base
-	return ret
+n = input() 
+for x in range(n):
+    input = raw_input()
+    row = ''
+    column = ''
+    base = 26
+    zero = ord('A')
 
-def aTot(zero,base,number):
-	ret = 0
-	for x in number: 
-		ret = ret*base + ord(x)-zero+1
+    if input[0] == 'R':
+        row,column = input.split('C')
+        column = int(column)
+        ret = ''
+        #change column into 26...
+        while column:
+            digital = (column-1)%base
+            ret = chr(digital+zero) + ret
+            column = (column-1)/base
+        print ret+row[1:]
+    else:
+        for i in xrange(0,len(input)):
+            if input[i] < 'A':
+                row = input[i:]
+                column = input[:i]
+                break
 
-	return ret
-
-if __name__ == "__main__":
-
-    n = input() 
-    for x in range(n):
-        input = raw_input()
-        row = ''
-        column = ''
-        if input[0] == 'R':
-            row,column = input.split('C')
-            column = int(column)
-            #change column into 26...
-            column_new = tToa(ord('A'),26,column)
-            print column_new+row[1:]
-        else:
-            for i in xrange(0,len(input)):
-                if input[i] < 'A':
-                    row = input[i:]
-                    column = input[:i]
-                    break
-
-            #change column into 10
-            column = aTot(ord('A'),26,column)
-            print 'R'+row+'C'+str(column)
+        #change column into 10
+        ret = 0
+        for x in column: 
+            ret = ret*base + ord(x)-zero+1
+        print 'R'+row+'C'+str(ret) 
